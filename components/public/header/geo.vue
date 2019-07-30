5669131db747a987b43c2004c32c335a237c97b2
tom <294274944@qq.com>
2019-07-30 20:13:47 +0800

    项目初始化完成

---
+++ components/public/header/geo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="m-geo">
+    <i class="el-icon-location"/>广州市
+    <nuxt-link class="changeCity" to="/changeCity">切换城市</nuxt-link>
+    [佛山 顺德 南沙]
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="scss">
+
+</style>
